12553e80596a8e7c9e03a79aa86f37a5ea9e0700
Mike Haiduk <x29a100@gmail.com>
2025-09-27 23:25:42 +0300

    цуа

--- src/main/java/org/vavilon/Main.java
+++ src/main/java/org/vavilon/Main.java
@@ -2,6 +2,6 @@ package org.vavilon;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!5");
+        System.out.println("Hello world!33");
     }
 }
